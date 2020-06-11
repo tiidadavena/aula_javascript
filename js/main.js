2fd7790dc4993b5cba7adbb9f287cbf08e8746c5
Juliana Barros Lima <juliana.barroslima@Julianas-MacBook-Pro.local>
2020-06-11 19:58:11 -0300

    Testando comandos de interação com o index.html

---
+++ js/main.js
@@ -0,0 +1,137 @@
+function botao() {
+	// body...
+	// alert("Obrigado por clicar!");
+	document.getElementById("tks").innerHTML = "Obrigado pela sua visita!";
+	// console.log(document.getElementById("tks"));
+
+
+}
+
+function redirecionar() {
+	// body...
+	window.open("https://google.com");
+}
+
+
+function trocarElemento(elemento) {
+	// body...
+	elemento.innerHTML = "SHIT";
+}
+
+function voltar(elemento) {
+	// body...
+	elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+	// body...
+	alert("página carregada");
+}
+
+function funcaoChange(elemento) {
+	// body...
+	console.log(elemento.value);
+}
+
+
+
+
+// // var cidade = "Houston";
+// // var citacao = "Roses are blue and violets are red"
+// // alert("Bem vinda" +  nome);
+// // console.log(nome);
+// // console.log(cidade);
+// // console.log(citacao.replace("blue", "red"));
+// // alert(citacao.replace("blue", "red"));
+// // console.log(cidade.toLowerCase());
+
+// // var lista = ["cubo", "prisma", "cilindro"];
+// // lista.push("esfera");
+// // console.log(lista.reverse());	
+// // console.log(lista.toString());
+// // console.log(lista.join("  - "));	
+
+// // var fruta = {nome: "maca", cor: "vermelha"};
+// // console.log(fruta);
+// // alert(fruta.cor);
+
+// // frutas = [{nome: "maca", cor: "vermelha"}, {nome: "uva", cor: "violeta"}];
+// // console.log(frutas);
+// // alert(frutas[1].cor);
+
+// // var idade = prompt("Qual a sua idade?");
+// // if (idade > 18){
+// // 	alert("maioridade penal");
+// // }else{
+// // 	alert("menor de idade");
+// // }
+// // var count = 0;
+// // while (count < 5){
+// // 	console.log(count);
+// // 	alert(count);
+// // 	count++;
+// // }
+
+// // var count =0;
+// // for (count = 0; count <= 5; count++) {
+// // 	alert(count);
+// // }
+
+// // var d = new Date();
+// // alert(d.getMonth()+1);
+// function soma(n1,n2){
+// 	return n1 + n2;
+// }
+
+// function validaIdade(idade) {
+// 	// body...
+// 	var validar;
+// 	if (idade >= 18){
+// 		validar = true;
+// 	}else{
+// 		validar = false;
+// 	}
+// 	return validar;
+// }
+
+// var idade = prompt("Qual a sua idade?");
+// console.log(validaIdade(idade));
+// // alert(soma(5,10));
+// // alert(setReplace("A China é continental", "China", "Canadá"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
